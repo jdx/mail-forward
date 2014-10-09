54eb7b2866dde5787468e87ad107485115baea37
dickeyxxx <jeff@dickeyxxx.com>
2014-10-08 18:05:09 -0700

    rename

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"crypto/rand"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+var TLSConfig *tls.Config
+
+func main() {
+	TLSConfig = setupTLS()
+	addr := "0.0.0.0:" + port()
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("listening on", addr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConn(&Client{
+			conn: conn,
+			in:   bufio.NewReader(conn),
+			out:  bufio.NewWriter(conn),
+		})
+	}
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "25"
+	}
+	return port
+}
+
+func setupTLS() *tls.Config {
+	cert, err := tls.LoadX509KeyPair("./certs/ssl.pem", "./certs/ssl.key")
+	if err != nil {
+		log.Println("Error loading certificate:", err)
+		return nil
+	}
+	config := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.VerifyClientCertIfGiven,
+		ServerName:   "mail.dickey.xxx",
+	}
+	config.Rand = rand.Reader
+	return config
+}
--- server.go
+++ server.go
@@ -2,56 +2,144 @@ package main
 
 import (
 	"bufio"
-	"crypto/rand"
 	"crypto/tls"
 	"fmt"
+	"io"
 	"log"
 	"net"
-	"os"
+	"strings"
+	"time"
 )
 
-var TLSConfig *tls.Config
+type Client struct {
+	conn net.Conn
+	in   *bufio.Reader
+	out  *bufio.Writer
+}
 
-func main() {
-	TLSConfig = setupTLS()
-	addr := "0.0.0.0:" + port()
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("listening on", addr)
+type Mail struct {
+	From string
+	To   []string
+	Data []string
+}
+
+var commands = map[string]func(c *Client, mail *Mail, input string) error{
+	"HELO":       cmdHelo,
+	"EHLO":       cmdEhlo,
+	"STARTTLS":   cmdStartTLS,
+	"NOOP":       cmdNoop,
+	"MAIL FROM:": cmdMailFrom,
+	"RCPT TO:":   cmdRcptTo,
+	"DATA":       cmdData,
+	"QUIT":       cmdQuit,
+}
+
+func handleConn(c *Client) {
+	defer c.conn.Close()
+	mail := &Mail{}
+	c.writeline("220 mail.dickey.xxx")
 	for {
-		conn, err := listener.Accept()
+		input, err := c.in.ReadString('\n')
+		fmt.Println("rcv:", input)
 		if err != nil {
-			panic(err)
+			log.Println(err)
+			return
 		}
-		go handleConn(&Client{
-			conn: conn,
-			in:   bufio.NewReader(conn),
-			out:  bufio.NewWriter(conn),
-		})
+		err = runCommand(c, mail, input)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			log.Println(err)
+			c.writeline("500 unexpected error")
+			return
+		}
 	}
 }
 
-func port() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "25"
+func runCommand(c *Client, mail *Mail, input string) error {
+	upperInput := strings.ToUpper(input)
+	for cmd, fn := range commands {
+		if strings.Index(upperInput, cmd) == 0 {
+			return fn(c, mail, input)
+		}
 	}
-	return port
+	return cmdUnknown(c, mail, input)
 }
 
-func setupTLS() *tls.Config {
-	cert, err := tls.LoadX509KeyPair("./certs/ssl.pem", "./certs/ssl.key")
+func cmdHelo(c *Client, mail *Mail, input string) error {
+	c.writeline("220 mail.dickey.xxx")
+	return nil
+}
+
+func cmdEhlo(c *Client, mail *Mail, input string) error {
+	c.writeline("250-mail.dickey.xxx")
+	c.writeline("250-SIZE 35882577")
+	//c.writeline("250-STARTTLS")
+	c.writeline("250-8BITMIME")
+	c.writeline("250-ENHANCEDSTATUSCODES")
+	c.writeline("250 SMTPUTF8")
+	return nil
+}
+
+func cmdStartTLS(c *Client, mail *Mail, input string) error {
+	c.writeline("220 Ready to start TLS")
+	tlsConn := tls.Server(c.conn, TLSConfig)
+	err := tlsConn.Handshake()
 	if err != nil {
-		log.Println("Error loading certificate:", err)
-		return nil
+		return err
 	}
-	config := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		ClientAuth:   tls.VerifyClientCertIfGiven,
-		ServerName:   "mail.dickey.xxx",
+	c.conn = tlsConn
+	c.in = bufio.NewReader(c.conn)
+	c.out = bufio.NewWriter(c.conn)
+	return nil
+}
+
+func cmdNoop(c *Client, mail *Mail, input string) error {
+	c.writeline("250 OK")
+	return nil
+}
+
+func cmdMailFrom(c *Client, mail *Mail, input string) error {
+	mail.From = input[8:]
+	c.writeline("250 Accepted")
+	return nil
+}
+
+func cmdRcptTo(c *Client, mail *Mail, input string) error {
+	mail.To = append(mail.To, input)
+	c.writeline("250 Accepted")
+	return nil
+}
+
+func cmdData(c *Client, mail *Mail, input string) error {
+	c.writeline("354 End data with <CR><LF>.<CR><LF>")
+	for {
+		c.conn.SetDeadline(time.Now().Add(time.Minute))
+		line, err := c.in.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		if line == ".\r\n" {
+			c.writeline("250 OK: Queued as 298892")
+			return nil
+		}
 	}
-	config.Rand = rand.Reader
-	return config
+}
+
+func cmdQuit(c *Client, mail *Mail, input string) error {
+	c.writeline("221 Bye")
+	return io.EOF
+}
+
+func cmdUnknown(c *Client, mail *Mail, input string) error {
+	c.writeline("500 Unrecognized command")
+	log.Println("Unrecognized command:", input)
+	return nil
+}
+
+func (c *Client) writeline(s string) {
+	c.out.WriteString(s + "\r\n")
+	c.out.Flush()
+	fmt.Println("snt:", s)
 }
